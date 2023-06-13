9ec0a2c34041e425513f21c711f9280d7803c848
student43120 <dsw43120@student.dsw.edu.pl>
2023-06-14 00:44:09 +0200

    initial

---
+++ public_html/app/GameWidget/Controller/Widget/index.php
@@ -0,0 +1,20 @@
+<?php
+namespace GameWidget\Controller\Widget;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class index implements ActionInterface {
+
+    protected $pageFactory;
+    public function __construct(
+        PageFactory $pageFactory
+    ){
+        $this->pageFactory = $pageFactory;
+    }
+    public function execute(){
+        return $this->pageFactory->create();
+    }
+}
+
+?>
